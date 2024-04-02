b5db1d55e945c4711131c83600d515f296a98ebd
Serhii Shtokal <serhiishtokal@gmail.com>
2024-04-02 18:43:20 +0200

    add react app Iframe testing

---
+++ ReactInIFramePlayground/Client/react-app/craco.config.js
@@ -0,0 +1,22 @@
+﻿const path = require('path');
+
+module.exports = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            // Change the output filename and chunkFilename for JS files
+            webpackConfig.output.filename = 'static/js/[name].custom.js';
+            webpackConfig.output.chunkFilename = 'static/js/[name].custom.chunk.js';
+
+            // Optionally, change CSS file names (if using MiniCssExtractPlugin)
+            const miniCssExtractPlugin = webpackConfig.plugins.find(
+                plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+            );
+            if (miniCssExtractPlugin) {
+                miniCssExtractPlugin.options.filename = 'static/css/[name].custom.css';
+                miniCssExtractPlugin.options.chunkFilename = 'static/css/[name].custom.chunk.css';
+            }
+
+            return webpackConfig;
+        },
+    },
+};
--- ReactInIFramePlayground/Client/react-app/src/App.tsx
+++ ReactInIFramePlayground/Client/react-app/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Frame from 'react-frame-component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <p>React App without frame. Have to be black regular font (as usual without styles) but it's overriden by website styles</p>
+        <Frame style={{border: "0px"}}>
+            <p style={{color: 'green'}}>Frame inside react App. Have to be green</p>
+        </Frame>
     </div>
   );
 }
